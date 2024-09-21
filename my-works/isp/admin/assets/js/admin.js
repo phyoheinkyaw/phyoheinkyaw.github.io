0596ab1413cce9d4a5cb7e2fd696799417e4fd0d
phyoheinkyaw <67408559+phyoheinkyaw@users.noreply.github.com>
2024-09-21 16:02:29 +0700

    delete admin

--- my-works/isp/admin/assets/js/admin.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('collapsed');
-        $('#content').toggleClass('collapsed');
-        $('#sidebarCollapse i').toggleClass('fa-circle-arrow-right');
-    });
-});
